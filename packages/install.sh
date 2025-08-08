60750c632a5a7650eff3a33b98f88f020566e4da
fjcrujeiras <fjcrujeiras@monom.ai>
2025-08-08 10:49:15 +0200

    feat: Added x220 to the equation

--- packages/install.sh
+++ packages/install.sh
@@ -6,6 +6,6 @@ export HOST=$(hostname)
 
 for file in ./group/${HOST}/*; do
 	echo "Installing $file"
-	sudo pacman -D --asexplicit --noconfirm - < "$file"
+	sudo pacman -S --asexplicit --noconfirm - < "$file"
 	paru -S --asexplicit --needed --noconfirm - < "$file"
 done
